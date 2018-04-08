a5c1c89b52ec4436c7ee28eb974da813f3abe012
mbj36 <mohitbajo36@gmail.com>
2018-04-09 00:40:07 +0530

    Github auth

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <nuxt-link to="/login">Login</nuxt-link>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-btn @click="login">Login with Github</v-btn>
+    <span v-if="$auth.$state.loggedIn">
+      <p>Hello world</p>
+    </span>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      login() {
+        this.$auth.loginWith('github')
+      }
+    }
+  }
+</script>
